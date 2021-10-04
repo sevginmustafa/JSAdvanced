4f0f57409cf3c4d5d1c89bbff64bfc1b0192c0a8
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2021-10-04 23:44:13 +0300

    add exercise

--- 04.DOM Introduction/Exercise/09. Number Convertor/solution.js
+++ 04.DOM Introduction/Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,23 @@
 function solve() {
+    const selectMenuTo = document.getElementById('selectMenuTo');
 
-    //TODO...
+    selectMenuTo.children[0].value = 'binary';
+    selectMenuTo.children[0].textContent = 'Binary';
+
+    const optionElement = document.createElement('option');
+    optionElement.value = 'hexadecimal';
+    optionElement.textContent = 'Hexadecimal';
+    selectMenuTo.appendChild(optionElement);
+
+    const resultButton = document.getElementsByTagName('button')[0];
+
+    resultButton.addEventListener('click', onclick);
+
+    function onclick() {
+        if (selectMenuTo.value == "binary") {
+            document.getElementById('result').value = Number(document.getElementById('input').value).toString(2);
+        } else {
+            document.getElementById('result').value = Number(document.getElementById('input').value).toString(16).toUpperCase();
+        }
+    }
 }
