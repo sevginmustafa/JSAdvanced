213fe73901e26dc41b7a8dfc6a8ff97fdef6de08
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2021-10-05 20:34:25 +0300

    add exercises

--- 05.DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
+++ 05.DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttons = document.getElementsByTagName('button');
+
+    for (const button of buttons) {
+        button.addEventListener('click', onclick);
+    }
+
+    function onclick(ev) {
+        const isLocked = ev.target.parentNode.querySelector('input[value="lock"]').checked;
+
+        if (!isLocked) {
+            const hiddenFields = ev.target.parentNode.querySelector('div');
+
+            if (hiddenFields.style.display == 'inline') {
+                hiddenFields.style.display = '';
+                ev.target.textContent = 'Show more';
+            }
+            else {
+                hiddenFields.style.display = 'inline'
+                ev.target.textContent = 'Hide it';
+            }
+        }
+    }
 }
--- 05.DOM Manipulations and Events/Exercise/04. Fill-Dropdown/dropdown.js
+++ 05.DOM Manipulations and Events/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    const newItemText = document.getElementById('newItemText');
+    const newItemValue = document.getElementById('newItemValue');
+
+    const optionElement = document.createElement('option');
+    optionElement.value = newItemValue.value;
+    optionElement.textContent = newItemText.value;
+
+    document.getElementById('menu').appendChild(optionElement);
+
+    newItemText.value='';
+    newItemValue.value='';
 }
