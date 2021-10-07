80163ba69cc06b8a9022c3bed45948aec1b7a0b5
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2021-10-07 16:19:48 +0300

    add exercise

--- 05.DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
+++ 05.DOM Manipulations and Events/Exercise/09. JavaScript-Quiz/solution.js
@@ -1,5 +1,5 @@
 function solve() {
-  const result = document.querySelector('.results-inner h1');
+  const result = document.querySelector('#results');
   let points = 0;
   const sections = document.getElementsByTagName('section');
 
@@ -23,18 +23,13 @@ function solve() {
     }
   }
 
-  if (points < 3) {
-    result.textContent = `You have ${points} right answers`;
-  } else {
-    result.textContent = 'You are recognized as top JavaScript fan!';
-  }
 
   function quiz1(ev) {
     if (ev.target.textContent.trim() == 'onclick') {
       points++;
     }
-    sections[0].classList.add('hidden');
-    sections[1].classList.remove('hidden');
+    sections[0].style.display = 'none';
+    sections[1].style.display = 'block';
 
     console.log(ev.target.textContent.trim())
   }
@@ -43,21 +38,22 @@ function solve() {
     if (ev.target.textContent.trim() == 'JSON.stringify()') {
       points++;
     }
-    sections[1].classList.add('hidden');
-    sections[2].classList.remove('hidden');
+    sections[1].style.display = 'none';
+    sections[2].style.display = 'block';
   }
 
   function quiz3(ev) {
     if (ev.target.textContent.trim() == 'A programming API for HTML and XML documents') {
       points++;
     }
+    sections[2].style.display = 'none';
 
-    const ul = sections[2].children[0];
+    result.style.display = 'block';
 
-    sections[2].removeChild(ul);
-
-    const newUl = document.querySelector('#results');
-
-    sections[2].appendChild(newUl);
+    if (points < 3) {
+      result.querySelector('h1').textContent = `You have ${points} right answers`;
+    } else {
+      result.querySelector('h1').textContent = 'You are recognized as top JavaScript fan!';
+    }
   }
 }
